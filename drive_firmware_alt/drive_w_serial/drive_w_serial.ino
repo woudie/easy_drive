ffdf5f2720fa3c8ecb4ab74f3a1b0a1ba84572f8
woudie <woudie@github.com>
2019-08-05 10:24:35 -0400

    Added battery monitoring on A0-A11

--- drive_firmware_alt/drive_w_serial/drive_w_serial.ino
+++ drive_firmware_alt/drive_w_serial/drive_w_serial.ino
@@ -6,6 +6,31 @@
 /* ROS imports */
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
+#include <std_msgs/Float32MultiArray.h>
+
+/**
+ * Arduino Mega Pin Definitions
+ * ================================================
+ * A0 -> Battery 1 Cell 1
+ * A1 -> Battery 1 Cell 2
+ * A2 -> Battery 1 Cell 3
+ * A3 -> Battery 1 Cell 4
+ * A4 -> Battery 1 Cell 5
+ * A5 -> Battery 1 Cell 6
+ * A6 -> Battery 2 Cell 1
+ * A7 -> Battery 2 Cell 2
+ * A8 -> Battery 2 Cell 3
+ * A9 -> Battery 2 Cell 4
+ * A10 -> Battery 2 Cell 5
+ * A11 -> Battery 2 Cell 6
+ */
+
+/**
+ * ===================================
+ *              NOTES
+ * ===================================
+ * Battery Monitor is untest, set use_bat to 0 to disable function
+ */
 
 SoftwareSerial SWSerial(NOT_A_PIN, 14);
 SoftwareSerial pinROS(NOT_A_PIN, 1);
@@ -16,6 +41,8 @@ void drive_callback(const geometry_msgs::Twist &drive_msg);
 #define BL 1
 #define BR 2
 
+int use_batt = 1;
+
 Sabertooth FrontST(128, SWSerial); //Address 128 Dip Switches (000111)
 Sabertooth RearST(129, SWSerial);  //Address 130 Dip Switches (000101) Address 129 did not work for some reason
 
@@ -25,6 +52,12 @@ Sabertooth RearST(129, SWSerial);  //Address 130 Dip Switches (000101) Address 1
 
 ros::NodeHandle nh;
 ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", drive_callback);
+
+std_msg::Float32MultiArray bat1[6];
+ros::Publisher pub1("battery_monitor/bat1", &bat1);
+
+std_msg::Float32MultiArray bat2[6];
+ros::Publisher pub2("battery_monitor/bat2", &bat2);
 
 void setWheelVelocity(int left, int right)
 {
@@ -49,17 +82,54 @@ void drive_callback(const geometry_msgs::Twist &drive_msg)
 void setup()
 {
   SWSerial.begin(9600); // 9600 is the default baud rate for Sabertooth packet serial.
-  //pinROS.begin(57600);
-  nh.getHardware()->setBaud(115200);
+  pinROS.begin(57600);
+  //nh.getHardware()->setBaud(115200);
   FrontST.autobaud();
   RearST.autobaud();
   
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
+  pinMode(A6, INPUT);
+  pinMode(A7, INPUT);
+  pinMode(A8, INPUT);
+  pinMode(A9, INPUT);
+  pinMode(A10, INPUT);
+  pinMode(A11, INPUT);
+
   nh.initNode();
   nh.subscribe(sub);
+
+  if( use_batt == 1){  
+    nh.advertise(pub1);
+    nh.advertise(pub2);
+  }
+}
+
+void battery_read(){
+    bat1[0] = analogRead(A0)
+    bat1[1] = analogRead(A1)
+    bat1[2] = analogRead(A2)
+    bat1[3] = analogRead(A3)
+    bat1[4] = analogRead(A4)
+    bat1[5] = analogRead(A5)
+    
+    bat2[0] = analogRead(A6)
+    bat2[1] = analogRead(A7)
+    bat2[2] = analogRead(A8)
+    bat2[3] = analogRead(A9)
+    bat2[4] = analogRead(A10)
+    bat2[5] = analogRead(A11)
 }
 
 void loop()
 {
+  if(use_batt == 1){
+    battery_read();
+  }
   nh.spinOnce();
   delay(1);
 }
